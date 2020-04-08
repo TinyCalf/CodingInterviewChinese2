cc898f2d3b086e61afbac2b1432a4805e64ef91e
zhujiasheng@happyplaygame.net <zhujiasheng@happyplaygame.net>
2020-04-08 15:40:53 +0800

    36

--- 36_ConvertBinarySearchTree/ConvertBinarySearchTree.cpp
+++ 36_ConvertBinarySearchTree/ConvertBinarySearchTree.cpp
@@ -21,38 +21,59 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 
 void ConvertNode(BinaryTreeNode* pNode, BinaryTreeNode** pLastNodeInList);
 
-BinaryTreeNode* Convert(BinaryTreeNode* pRootOfTree)
-{
-    BinaryTreeNode *pLastNodeInList = nullptr;
-    ConvertNode(pRootOfTree, &pLastNodeInList);
+// BinaryTreeNode* Convert(BinaryTreeNode* pRootOfTree)
+// {
+//     BinaryTreeNode *pLastNodeInList = nullptr;
+//     ConvertNode(pRootOfTree, &pLastNodeInList);
 
-    // pLastNodeInList指向双向链表的尾结点，
-    // 我们需要返回头结点
-    BinaryTreeNode *pHeadOfList = pLastNodeInList;
-    while(pHeadOfList != nullptr && pHeadOfList->m_pLeft != nullptr)
-        pHeadOfList = pHeadOfList->m_pLeft;
+//     // pLastNodeInList指向双向链表的尾结点，
+//     // 我们需要返回头结点
+//     BinaryTreeNode *pHeadOfList = pLastNodeInList;
+//     while(pHeadOfList != nullptr && pHeadOfList->m_pLeft != nullptr)
+//         pHeadOfList = pHeadOfList->m_pLeft;
 
-    return pHeadOfList;
+//     return pHeadOfList;
+// }
+
+// void ConvertNode(BinaryTreeNode* pNode, BinaryTreeNode** pLastNodeInList)
+// {
+//     if(pNode == nullptr)
+//         return;
+
+//     BinaryTreeNode *pCurrent = pNode;
+
+//     if (pCurrent->m_pLeft != nullptr)
+//         ConvertNode(pCurrent->m_pLeft, pLastNodeInList);
+
+//     pCurrent->m_pLeft = *pLastNodeInList; 
+//     if(*pLastNodeInList != nullptr)
+//         (*pLastNodeInList)->m_pRight = pCurrent;
+
+//     *pLastNodeInList = pCurrent;
+
+//     if (pCurrent->m_pRight != nullptr)
+//         ConvertNode(pCurrent->m_pRight, pLastNodeInList);
+// }
+
+BinaryTreeNode* Convert(BinaryTreeNode* pRootOfTree) {
+    ConvertCore(pRootOfTree, nullptr);
+    return pRootOfTree;
 }
 
-void ConvertNode(BinaryTreeNode* pNode, BinaryTreeNode** pLastNodeInList)
-{
-    if(pNode == nullptr)
-        return;
+BinaryTreeNode* ConvertCore(BinaryTreeNode* pRoot, BinaryTreeNode* pBefore) {
+    if(pRoot == nullptr) 
+        return nullptr;
 
-    BinaryTreeNode *pCurrent = pNode;
+    BinaryTreeNode* pLeftLast = ConvertCore(pRoot->m_pLeft, pRoot);
+    BinaryTreeNode* pRightLast = pLeftLast ? 
+        ConvertCore(pRoot->m_pRight, pRoot) :
+        ConvertCore(pRoot->m_pRight, pLeftLast);
 
-    if (pCurrent->m_pLeft != nullptr)
-        ConvertNode(pCurrent->m_pLeft, pLastNodeInList);
+    pRoot->m_pLeft = pBefore;
+    if(pBefore != nullptr)
+        pBefore->m_pRight = pRoot;
 
-    pCurrent->m_pLeft = *pLastNodeInList; 
-    if(*pLastNodeInList != nullptr)
-        (*pLastNodeInList)->m_pRight = pCurrent;
-
-    *pLastNodeInList = pCurrent;
-
-    if (pCurrent->m_pRight != nullptr)
-        ConvertNode(pCurrent->m_pRight, pLastNodeInList);
+    return pRight || pLeftLast || pRoot;
 }
 
 // ====================测试代码====================
